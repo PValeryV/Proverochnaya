e62f774883aa6e8210a4246afa6562db5b5e8591
Valerii Prelovskii <Valeriy.Prelovskiy@gmail_point_com>
2022-09-17 23:24:16 +0300

    написана программа

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,1 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
+int schet(string[] str,int n){
+    int count =0;
+    for (int i=0;i<str.Length;i++)
+        if(str[i].Length<=n)count++;
+    return count;
+}
+
+void print(string[] itog){
+    Console.Write("[");
+    for (int i=0;i<itog.Length;i++){
+        Console.Write($"\"{itog[i]}\"");
+        if ((i+1)!=itog.Length) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+
+void fill(ref string [] itog,string [] str, int n){
+    int count =0;
+    for (int i=0;i<str.Length;i++)
+        if(str[i].Length<=n){
+            itog[count] = str[i];
+            count++;
+        }
+
+}
+
+string [] str = {"hello","2","world",":-)"}; // - тут можно задать файл
+int n = 3;  //не больше этого числа символов будут состоять строки в новом массиве
+
+string [] itog = new string[schet(str,n)];
+fill(ref itog,str,n);
+print(itog);
+
